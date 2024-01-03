61dde4a0d485ec06911444206e17707b78972dc3
bozen <burakozen91@gmail.com>
2024-01-03 23:28:02 +0300

    dockerFile added

--- hello_world_main.py
+++ hello_world_main.py
@@ -3,7 +3,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         app="hello_world_app.hello_world_app:app",
-        host="localhost",
+        host="0.0.0.0",
         port=8000,
         reload=True
 
